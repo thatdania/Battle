3c224a1c55b72335ccf4c46daf76ed21f0ce15ed
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-11-08 16:44:03 +0000

    Adds attacks and confirmations

--- app.rb
+++ app.rb
@@ -13,10 +13,10 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1 = session[:player_1]
-    @player_2 = session[:player_2]
-    @p1_hp = 0
-    @p2_hp = 0
+    set_players
+    session[:p1_hp] = 0
+    session[:p2_hp] = 0
+    set_hp
     erb(:play)
   end
 
@@ -26,6 +26,30 @@ class Battle < Sinatra::Base
     redirect '/play'
   end
 
+  post '/attack_P1' do
+    set_players
+    session[:p1_hp] += 20
+    set_hp
+    @message = 'You have attacked player 1, well done'
+    erb(:attack)
+  end
 
+  post '/attack_P2' do
+    set_players
+    session[:p2_hp] += 20
+    set_hp
+    @message = 'You have attacked player 2, well done'
+    erb(:attack)
+  end
+
+  def set_players
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+  end
+
+  def set_hp
+    @p1_hp = session[:p1_hp]
+    @p2_hp = session[:p2_hp]
+  end
   run! if app_file == $0
 end
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe 'Battle', :type => :feature do
-  it 'Testing infrastructure' do
-    visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
+  # before(:each) { sign_in_and_play }
+
+  # it 'Testing infrastructure' do
+  #   visit '/'
+  #   expect(page).to have_content 'Testing infrastructure working!'
+  # end
 
   it 'accepts two player names' do
     sign_in_and_play
@@ -14,5 +16,29 @@ describe 'Battle', :type => :feature do
   it 'show\'s players hit points' do
     sign_in_and_play
     expect(page).to have_content('P1: 0/100 P2: 0/100')
+  end
+
+  it 'allows attacks from player 1' do
+    sign_in_and_play
+    click_on('Attack P2')
+    expect(page).to have_content('P1: 0/100 P2: 20/100')
+  end
+
+  it 'confirms attack on player 2' do
+    sign_in_and_play
+    click_on('Attack P2')
+    expect(page).to have_content('You have attacked player 2, well done')
+  end
+
+  it 'allows attacks from player 2' do
+    sign_in_and_play
+    click_on('Attack P1')
+    expect(page).to have_content('P1: 20/100 P2: 0/100')
+  end
+
+  it 'confirms attack on player 1' do
+    sign_in_and_play
+    click_on('Attack P1')
+    expect(page).to have_content('You have attacked player 1, well done')
   end
 end
